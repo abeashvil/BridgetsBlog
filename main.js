6f1ceba422d344e02c93a407dfab0f85f069d4fa
THHSGit <31997626+THHSGit@users.noreply.github.com>
2018-01-27 11:08:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+//let username = "jishybaddy123";
+//let password = "Tennis12";
+let user = $("#un").val();
+let pass = $("#pw").val();
 
+$("#sign_in").click(function(){
+if (user.length != 13){
+                alert("Incorrect username! Please try again.");
+}
+if (pass.length != 8){
+                alert("Incorrect password! Please try again.");
+}
+  if (pass.length == 8 && user.length == 13){
+               $.mobile.navigate( "#mainpage" ,  {transition:"pop"});
+  }
